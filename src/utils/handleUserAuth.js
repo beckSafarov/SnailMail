4dba70228bbcec58418223df96a5a9eaaf4075f6
Begzod Safarov <begzod.safarov07@gmail.com>
2022-11-24 11:28:19 +0500

    bug fix

--- src/utils/handleUserAuth.js
+++ src/utils/handleUserAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async () => {
   try {
-    const res = await axios.get('/api/auth/me')
+    const res = await axios.get('/auth/me')
     return { success: true, data: res.data.data }
   } catch (error) {
     return { success: false, data: error }
