eee6339f77ac55489ec845e07d6182a9bc3e4cfc
Begzod Safarov <begzod.safarov07@gmail.com>
2022-11-30 16:09:02 +0500

    MailsContext context

---
+++ src/context/MailsContext.js
@@ -0,0 +1,73 @@
+import React, { createContext, useReducer } from 'react'
+import axios from 'axios'
+import { baseUrl } from '../constants';
+
+
+const initialState = {
+  users: [],
+  mails: []
+}
+
+//create context
+export const MailsContext = createContext(initialState);
+
+const MailsReducer = (state, {type, payload}) => {
+  switch (type) {
+    case 'loading':
+      return {
+        ...state, 
+        loading: true
+      }
+    case 'setUsers':
+      return {
+        ...state,
+        loading: false,
+        users: payload
+      }
+    case 'setMails':
+      return { ...state, loading: false, mails: payload }
+    case 'error':
+      return {
+        ...state, 
+        loading: false, 
+        error: payload
+      }
+    default:
+      return state
+  }
+}
+
+export const MailsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(MailsReducer, initialState);
+
+  const getUsers = async () => {
+    dispatch({ type: 'loading' })
+    try {
+      const res = await axios.get(`${baseUrl}/users`)
+      dispatch({ type: 'setUsers', payload: res.data.data })
+    } catch (error) {
+      dispatch({ type: 'error', payload: error })
+    }
+  }
+  
+  const getMails = async (user) => {
+    dispatch({ type: 'loading' })
+    try {
+      const res = await axios.get(`${baseUrl}/users&user1=${user.id}`)
+      dispatch({ type: 'setUsers', payload: res.data.data })
+    } catch (error) {
+      dispatch({ type: 'error', payload: error })
+    }
+  }
+
+
+  return (
+    <MailsContext.Provider value={{
+      ...state,
+      getUsers,
+      getMails
+    }}>
+      {children}
+    </MailsContext.Provider>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './globals.css';
 import App from './App';
 import {AuthProvider} from './context/AuthContext'
+import { MailsProvider } from './context/MailsContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <MailsProvider>
+        <App />
+      </MailsProvider>
     </AuthProvider>
   </React.StrictMode>
 );
